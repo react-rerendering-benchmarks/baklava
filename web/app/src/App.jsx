471b95d54ae2a8d34c352de1dca626d4fdbf5a58
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-13 10:30:45 -0400

    Instrumenting React Components

--- web/app/src/App.jsx
+++ web/app/src/App.jsx
@@ -2,6 +2,7 @@ import { memo } from "react";
 import { useState } from 'react';
 import './style.css';
 const App = memo(function App() {
+  console.log(window.globalCount++);
   return <>
     <h1>Baklava TD Web Game</h1> 
    </>;
